a50c821321309c0a9812fe0f780e298436bf5f4f
angseung <angseung@sju.ac.kr>
2021-07-22 10:37:48 +0900

    Added elapse time estimator...

--- miv_main.py
+++ miv_main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sklearn.datasets import load_wine
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -13,21 +14,28 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 lr = LogisticRegression(max_iter=100, verbose=0, solver='liblinear')
 
+lr_train_pre = datetime.now()
 lr.fit(X_train, y_train)
-print(lr.score(X_train, y_train))
+lr_train_af = datetime.now()
+
+print(lr.score(X_train, y_train), lr_train_af - lr_train_pre)
 print(lr.score(X_test, y_test))
 
 miv = MIV(Model=lr,
           threshold=0.85,
           zeta=0.1,
           is_clf=True)
+
 miv.fit(X_train, y_train)
 
 X_train_miv = X_train[:, miv.selected_idx]
 X_test_miv = X_test[:, miv.selected_idx]
 
 lr_miv = LogisticRegression(max_iter=100, verbose=0, solver='liblinear')
-lr_miv.fit(X_train_miv, y_train)
 
-print(lr_miv.score(X_train_miv, y_train))
+lr_train_pre = datetime.now()
+lr_miv.fit(X_train_miv, y_train)
+lr_train_af = datetime.now()
+
+print(lr_miv.score(X_train_miv, y_train), lr_train_af - lr_train_pre)
 print(lr_miv.score(X_test_miv, y_test))
